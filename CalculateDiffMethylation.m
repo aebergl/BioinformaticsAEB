72b4857bd34dc20ce3b6f0b1929fa75405042f08
aebergl <aebergl@gmail.com>
2023-01-09 16:01:56 -0500

    updates

--- CalculateDiffMethylation.m
+++ CalculateDiffMethylation.m
@@ -33,8 +33,10 @@ end
 % DATA.X=B2M(DATA.X);
 % DATA_Median.X=B2M(DATA_Median.X);
 
+median_x = median(DATA_Median.X(:,indx_ToUse),1,'omitnan');
 
-X = bsxfun(@minus, DATA.X(:,indx_ToUse), median(DATA_Median.X(:,indx_ToUse),1,'omitnan'));
+
+X = bsxfun(@minus, DATA.X(:,indx_ToUse),median_x );
 
 Chr = DATA.ColAnnotation(indx_ToUse,ChrCol);
 
@@ -48,15 +50,35 @@ X_pos(indx_pos) = X(indx_pos);
 X_neg(indx_neg) = X(indx_neg);
 
 
+% Fin max Hyper and min Hypo
+
+X_Lim = [ones(1,size(X,2));zeros(1,size(X,2))];
+X_Lim = bsxfun(@minus, X_Lim, median_x);
+
+X_Lim_pos = ones(size(X_Lim)) * NaN;
+X_Lim_neg = ones(size(X_Lim)) * NaN;
+
+indx_pos_max = X_Lim > CutOff_Val;
+indx_neg_min = X_Lim < -CutOff_Val;
+
+
+X_Lim_pos(indx_pos_max) = X_Lim(indx_pos_max);
+X_Lim_neg(indx_neg_min) = X_Lim(indx_neg_min);
+
+
+x_max = sum(X_Lim_pos(1,:),2,'omitnan');
+x_min = sum(X_Lim_neg(2,:),2,'omitnan');
+
+
 RESULTS_DATA.X(:,1) = mean(DATA.X(:,indx_ToUse),2,'omitnan');
-RESULTS_DATA.X(:,2) = sum(X_pos,2,'omitnan');
-RESULTS_DATA.X(:,3) = sum(X_neg,2,'omitnan');
-RESULTS_DATA.X(:,4) = RESULTS_DATA.X(:,2) ./ sum(indx_pos,2);
-RESULTS_DATA.X(:,5) = RESULTS_DATA.X(:,3) ./ sum(indx_neg,2);
+RESULTS_DATA.X(:,2) = sum(X_pos,2,'omitnan') ./ x_max;
+RESULTS_DATA.X(:,3) = sum(X_neg,2,'omitnan') ./ x_min;
+RESULTS_DATA.X(:,4) = mean(X_pos,2,'omitnan');
+RESULTS_DATA.X(:,5) = mean(X_neg,2,'omitnan');
 ChrTxt = {'1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22'};
 for i = 1:length(ChrTxt)
     indx = strcmp(ChrTxt(i),Chr);
-    RESULTS_DATA.X(:,i+5) = sum(X_pos(:,indx),2,'omitnan');
+    RESULTS_DATA.X(:,i+5) = sum(X_pos(:,indx),2,'omitnan') ./ sum(X_Lim_pos(1,indx),2,'omitnan');
 end
  ChrTxt = {'Chr.01','Chr.02','Chr.03','Chr.04','Chr.05','Chr.06','Chr.07','Chr.08','Chr.09','Chr.10','Chr.11','Chr.12','Chr.13','Chr.14','Chr.15','Chr.16','Chr.17','Chr.18','Chr.19','Chr.20','Chr.21','Chr.22'}';
 ChrTxt=strcat({'Hyper '},ChrTxt);
--- Chr_Survival_Dot_Plot.m
+++ Chr_Survival_Dot_Plot.m
@@ -1,4 +1,4 @@
-function fh  = Chr_Survival_Dot_Plot(DATA,X_Variable,SizeType,fWidth,fHight,LegendPVals,LegendSizeVal)
+function fh  = Chr_Survival_Dot_Plot(DATA,X_Variable,Y_Samples,SizeType,SizeCutOffs,LegendSizeVal,fWidth,fHight)
 
 FontSize = 8;
 minSize = 20;
@@ -11,14 +11,17 @@ GridLines = 'on';
 % fWidth = 7;
 % fHight = 2.5;
 
-if isempty(nGroups)
-    nGroups = length(DATA.PATHWAYS.q);
-end
 
+if isempty(Y_Samples)
+    SampleIndx = ones(DATA.nRow,1);
+else 
+    SampleIndx = ismember(DATA.RowId,Y_Samples);
+end
+nGroups = sum (SampleIndx);
 
 indx_X_Val = strcmpi(X_Variable,DATA.ColId);
 if any(indx_X_Val)
-    xVal = log2(DATA.X(:,indx_X_Val));
+    xVal = log2(DATA.X(SampleIndx,indx_X_Val));
     xValTxt = 'log_2(HR)';
 else
     error('%s not found',X_Variable)
@@ -27,29 +30,36 @@ end
 % Select Size value
 indx_SizeVal = strcmpi(SizeType,DATA.ColId);
 if any(indx_SizeVal)
-    SizeVal = DATA.X(indx_Chr,indx_SizeVal);
+    SizeValOrig = DATA.X(SampleIndx,indx_SizeVal);
+    SizeVal = -log10(SizeValOrig);
+    %LegendPValsPlot = -log10(SizeCutOffs);
 else
     error('%s not found',SizeType)
 end
 
 
-SizeVal = DATA.PATHWAYS.numGenesInOveralap(1:nGroups);
-ColorVal = DATA.PATHWAYS.Ratio(1:nGroups);
-ColorlabelTxt = 'GeneRatio';
-YtickLabelTxt = DATA.PATHWAYS.Description(1:nGroups);
-YtickLabelTxt = DATA.PATHWAYS.Name(1:nGroups);
-YtickLabelTxt = strrep(YtickLabelTxt,'_TARGET_GENES','');
-YtickLabelTxt=strcat('\it',YtickLabelTxt);
+LegendSizeValMat = sum(repmat(SizeCutOffs,nGroups,1) >= repmat(SizeValOrig,1,length(SizeCutOffs)),2);
+
+SizeValPlot = LegendSizeVal(LegendSizeValMat);
 
 
-Cmap = colorcet('L08');
-CLim = [0 ceil((max(ColorVal)+0.001)*100)/100];
-Cmap = flipud(Cmap);
-Cmap = colormap('winter');
+%SizeVal = DATA.PATHWAYS.numGenesInOveralap(1:nGroups);
+% ColorVal = DATA.PATHWAYS.Ratio(1:nGroups);
+% ColorlabelTxt = 'GeneRatio';
+% YtickLabelTxt = DATA.PATHWAYS.Description(1:nGroups);
+% YtickLabelTxt = DATA.PATHWAYS.Name(1:nGroups);
+% YtickLabelTxt = strrep(YtickLabelTxt,'_TARGET_GENES','');
+% YtickLabelTxt=strcat('\it',YtickLabelTxt);
+
+
+% Cmap = colorcet('L08');
+% %CLim = [0 ceil((max(ColorVal)+0.001)*100)/100];
+% Cmap = flipud(Cmap);
+% Cmap = colormap('winter');
 %colormap(cmap);
 
-SizeValPlot = rescale(SizeVal,minSize,maxSize);
-LegendSizeValPlot = rescale(LegendSizeVal,minSize,maxSize);
+% SizeValPlot = rescale(SizeVal,minSize,maxSize);
+% LegendPValsPlot = rescale(LegendPValsPlot,minSize,maxSize);
 
 minVal=min(xVal);
 maxVal=max(xVal);
@@ -66,27 +76,34 @@ ah.YGrid = GridLines;
 ah.YDir = 'Reverse';
 ah.YTick = 1:nGroups;
 ah.YLim = [0.5 nGroups+0.5];
-ah.YTickLabel = YtickLabelTxt;
-ah.Colormap = Cmap;
-ah.CLim = CLim;
+ah.YTickLabel = Y_Samples;
+% ah.Colormap = Cmap;
+% ah.CLim = CLim;
 ah.XLim =[minVal-nudgeVal maxVal+nudgeVal];
 ah.OuterPosition(3:4) = [fWidth-(fWidth/10) fHight];
 ah.TickLength=[ 0.05/nGroups    0.0];
 xlabel(ah,xValTxt);
 
-sh = scatter(xVal,1:nGroups,SizeValPlot,ColorVal,'MarkerFaceColor','flat','MarkerEdgeColor',[0.1 0.1 0.1]);
+sh = scatter(xVal,1:nGroups,SizeValPlot,[1 0 0],'MarkerFaceColor','flat','MarkerEdgeColor',[0.1 0.1 0.1]);
 
-YPos = 1:1.5:1.5*length(LegendSizeValPlot);
+YPos = 1:1.5:1.5*length(LegendSizeVal);
 
-shl = scatter(ah.XLim(2)+nudgeVal,YPos+1,LegendSizeValPlot,[0 0 0]);
-text(ah.XLim(2)+1.4*nudgeVal,1,'Count','HorizontalAlignment','center','VerticalAlignment','middle','FontSize',FontSize)
-for i = 1:length(LegendSizeValPlot)
-    text(ah.XLim(2)+1.7*nudgeVal,YPos(i)+1,sprintf('%u',LegendSizeVal(i)),'HorizontalAlignment','left','VerticalAlignment','middle','FontSize',FontSize)
+shl = scatter(ah.XLim(2)+nudgeVal,YPos+1,LegendSizeVal,[0 0 0]);
+text(ah.XLim(2)+1.4*nudgeVal,1,'p-value','HorizontalAlignment','center','VerticalAlignment','middle','FontSize',FontSize)
+SizeCutOffs
+for i = 1:length(LegendSizeVal)
+    if i==1;
+        txt_str = 'N.S.'
+    else
+        n_val = floor(-log10(SizeCutOffs(i))) + 1;
+        txt_str = sprintf('%.*f',n_val,SizeCutOffs(i));
+    end
+    text(ah.XLim(2)+1.7*nudgeVal,YPos(i)+1,txt_str,'HorizontalAlignment','left','VerticalAlignment','middle','FontSize',FontSize)
 end
 
-ch = colorbar(ah);
-ch.Label.String=ColorlabelTxt;
-ch.Units="inches";
-ch.FontSize=FontSize;
-ch.Position=[ah.OuterPosition(1) + ah.OuterPosition(3)+0.02, ah.Position(2) 0.2, fHight/2.5];
+% ch = colorbar(ah);
+% ch.Label.String=ColorlabelTxt;
+% ch.Units="inches";
+% ch.FontSize=FontSize;
+% ch.Position=[ah.OuterPosition(1) + ah.OuterPosition(3)+0.02, ah.Position(2) 0.2, fHight/2.5];
 
