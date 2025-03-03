6d856a1c8732811c8171667a35152746ee8fe34d
aebergl <aebergl@gmail.com>
2025-03-03 11:01:24 -0500

    updates

--- Geo2SampleInfo.m
+++ Geo2SampleInfo.m
@@ -1,4 +1,24 @@
-function [SampleInfo, ColumnNames] = Geo2SampleInfo(GEO,FileOut)
+function [SampleInfo, FieldNames, HeaderInfo] = Geo2SampleInfo(GEO,FileOut)
+% USAGE:
+%   SampleInfo, ColumnNames, HeaderInfo] = Geo2SampleInfo(GEO,FileOut)
+%   process a GEO file ro readable data
+%
+% INPUTS:
+%   DATA:       GEO structure
+%   FileOut:    FilenName for output results, [] will not write the results to a file
+% OUTPUTS:
+%   SampleInfo: String array with sample info
+%   FieldNames: String arrays with extrated fields
+%   HeaderInfo: Information strucure about the series
+%
+%   options ---------------------------------------
+%
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% by Anders Berglund, 2025 aebergl at gmail.com                           %
+
+% Series info
+HeaderInfo = GEO.Header.Series;
 
 Samplefields = fieldnames(GEO.Header.Samples);
 numSamplefields = length(Samplefields);
@@ -12,7 +32,7 @@ numTotalSampleFields=sum(tmp);
 
 SampleInfo = cell(numSamples,numTotalSampleFields);
 SampleInfo(:) = {'---'};
-ColumnNames = cell(numTotalSampleFields,1);
+FieldNames = cell(numTotalSampleFields,1);
 
 counter = 0;
 for i = 1:numSamplefields
@@ -21,7 +41,7 @@ for i = 1:numSamplefields
 
     if nVal == 1
         counter = counter + 1;
-        ColumnNames(counter) = Samplefields(i);
+        FieldNames(counter) = Samplefields(i);
         SampleInfo(:,counter) = Val(1:numSamples);
     else
         for j=1:nVal
@@ -32,7 +52,7 @@ for i = 1:numSamplefields
                 if ~isempty(Val_1)
                     indx=strfind(Val_1,':');
                     if ~AddNameFlag
-                        ColumnNames{counter} = Val_1(1:indx-1);
+                        FieldNames{counter} = Val_1(1:indx-1);
                         AddNameFlag = true;
                     end
                     Val_1 =  Val_1(indx+1:end);
@@ -44,17 +64,18 @@ for i = 1:numSamplefields
     end
 end
 
+%Remove all fields that have the same value
 SameValIndx = false(numTotalSampleFields,1);
 for i = 1:numTotalSampleFields
-    %unique(SampleInfo(:,i))
     if length(unique(SampleInfo(:,i))) == 1
         SameValIndx(i) = true;
     end
 end
 
-ColumnNames(SameValIndx) = [];
+FieldNames(SameValIndx) = [];
 SampleInfo(:,SameValIndx) = [];
 
+%Write results to a text file
 if ~isempty(FileOut)
     [fid,message] = fopen(FileOut,'w');
     if  fid == -1
@@ -62,8 +83,8 @@ if ~isempty(FileOut)
         disp(message)
         return
     end
-    fprintf(fid,'%s\t',ColumnNames{1:end-1});
-    fprintf(fid,'%s\n',ColumnNames{end});
+    fprintf(fid,'%s\t',FieldNames{1:end-1});
+    fprintf(fid,'%s\n',FieldNames{end});
     for i=1:numSamples
         fprintf(fid,'%s\t',SampleInfo{i,1:end-1});
         fprintf(fid,'%s\n',SampleInfo{i,end});
