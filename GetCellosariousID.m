9a43de4c3d3971f94f8a30e4ba45bf8675f5fc1b
aebergl <aebergl@gmail.com>
2025-03-05 16:40:57 -0500

    Updates

--- GetCellosariousID.m
+++ GetCellosariousID.m
@@ -1,19 +1,78 @@
-function [RESULTS, OutputFields] = GetCellosariousID(IdList,CelloDATA,OutputFields,ManualCorrection,HumanOnly)
-CleanChar = {' ','-', '_'};
-ReplaceChar = "";
+function [RESULTS, OutputFields] = GetCellosariousID(IdList,CelloDATA,varargin)
+% USAGE:
+% [RESULTS, OutputFields] = GetCellosariousID(IdList,CelloDATA,varargin)
+% Finds the correct identifier for IdList using Cellosaurus data
+%
+% INPUTS:
+% IdList:       List of Cell Libe Ids to be converted
+% CelloDATA:    Data structure with Cellosaurus data
+%
+% OUTPUTS:
+% RESULTS: Figure handle to dot plot figure
+%
+%   options ---------------------------------------
+%
+%   'OutputFields'      Cell with names ot output fields to export, [] uses all
+%   'ManualCorrection'  N*2 with orig Id and New Id to be used instead
+%   'HumanOnly'         true/false if only human cell lines should be exported [true]
+%   'CleanChar'         Cell with charatcers to be removed when cleaning [{' ','-', '_'}]
+%   'PrintResults'      File name for exported results
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% by Anders Berglund, 2025 aebergl at gmail.com                           %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+OutputFields =      [];
+ManualCorrection =  [];
+HumanOnly =         true;
+CleanChar =         {' ','-', '_'};
+ReplaceChar =       "";
+PrintResults =      false;
+% Check input
+if nargin > 2
+    ArgsList = {'OutputFields','ManualCorrection','HumanOnly','CleanChar','PrintResults'};
+    for j=1:2:numel(varargin)
+
+        ArgType = varargin{j};
+        ArgVal = varargin{j+1};
+        if ~strncmpi(ArgType,ArgsList,numel(ArgType))
+            error('Invalid input option: %s', ArgType);
+        else
+            switch lower(ArgType)
+                case 'outputfields'
+                    OutputFields = ArgVal;
+                case 'manualcorrection'
+                    ManualCorrection = ArgVal;
+                case 'humanonly'
+                    HumanOnly =ArgVal;
+                case 'cleanchar'
+                    CleanChar = ArgVal;
+                case 'printresults'
+                    PrintResults = true;
+                    FileOut = ArgVal;
+            end
+
+        end
+    end
+end
+
 n = length(IdList);
 
 if isempty(OutputFields)
     OutputFields = CelloDATA.ColumnFields;
 end
 
-[~, OutputFieldsIndx, ResultsIndx] = intersect(OutputFields,CelloDATA.ColumnFields,'stable')
+[~, OutputFieldsIndx, ResultsIndx] = intersect(OutputFields,CelloDATA.ColumnFields,'stable');
 
-if length(OutputFields) ~= length(OutputFieldsIndx);
-   warning('Not all OutputFields found in CELLOSAURUS.ColumnFields') 
+if length(OutputFields) ~= length(OutputFieldsIndx)
+    warning('Not all OutputFields found in CELLOSAURUS.ColumnFields')
 end
 
-RESULTS = strings(n,length(OutputFields)+2);
+OutputFields(end+1) = {'Match Type'};
+
+OutputFields = [{'Orig Id'},OutputFields];
+
+RESULTS = strings(n,length(OutputFields));
 
 
 PrimaryId = CelloDATA.CellData(:,strcmp('ID',CelloDATA.ColumnFields));
@@ -30,9 +89,9 @@ EntryNumHuman = find(indx_human);
 
 for i = 1:n
     Id = IdList(i);
-    Id_Clean = replace(Id,CleanChar,ReplaceChar); 
+    Id_Clean = replace(Id,CleanChar,ReplaceChar);
     RESULTS(i,1) = Id;
-    
+
     % Cheick if Id is in Manual Correction array
     if ~isempty(ManualCorrection)
         indx_MC = strcmp(Id,ManualCorrection(:,1));
@@ -45,7 +104,6 @@ for i = 1:n
     indx = strcmp(Id,PrimaryId) & indx_human;
 
     if any(indx) % Should always be une unique hit
-        RESULTS(i,2) = PrimaryId(indx);
         RESULTS(i,2:end-1) = CelloDATA.CellData(indx,ResultsIndx);
         RESULTS(i,end) = "Primary Id match";
     else
@@ -54,10 +112,7 @@ for i = 1:n
         indx = strcmp(Id,CelloDATA.Synonyms);
         EntryNum = CelloDATA.SynonymsIndx(indx);
         if (sum(indx) == 1 || isscalar(unique((CelloDATA.SynonymsIndx(indx))))) && ismember(EntryNum,EntryNumHuman)% Only one unique Hit
-            
-            RESULTS(i,2) = PrimaryId(EntryNum);
-            % RESULTS(i,3) = CelloDATA.CellLine(EntryNum).AC;
-            % RESULTS(i,4) = CelloDATA.CellLine(EntryNum).OX;
+            RESULTS(i,2:end-1) = CelloDATA.CellData(EntryNum,ResultsIndx);
             RESULTS(i,end) = "Unique match to synonym";
 
         elseif sum(indx) > 1 % Multiple hits
@@ -65,20 +120,13 @@ for i = 1:n
             OX = CelloDATA.CellData(EntryNum,strcmp('OX',CelloDATA.ColumnFields));
             indx_hum = strcmp('NCBI_TaxID=9606; ! Homo sapiens (Human)',OX);
             if sum(indx_hum) == 1
-                RESULTS(i,2) = PrimaryId(EntryNum(indx_hum));
-                % RESULTS(i,3) = CelloDATA.CellLine(EntryNum(indx_hum)).AC;
-                % RESULTS(i,4) = CelloDATA.CellLine(EntryNum(indx_hum)).OX;
+                RESULTS(i,2:end-1) = CelloDATA.CellData(EntryNum(indx_hum),ResultsIndx);
                 RESULTS(i,end) = "Multiple match to synonym, only one human";
 
             else
-                % Check if ID works with a cleaned Primary ID
-                % ID_clean_tmp = [CelloDATA.CellLine(EntryNum).ID]';
-                % ID_clean_tmp = replace(ID_clean_tmp,CleanChar,ReplaceChar);
                 indx_id = strcmp(Id_Clean,PrimaryIdClean(EntryNum));
                 if sum(indx_id) == 1
-                    RESULTS(i,2) = PrimaryId(EntryNum(indx_id));
-                    % RESULTS(i,3) = CelloDATA.CellLine(EntryNum(indx_id)).AC;
-                    % RESULTS(i,4) = CelloDATA.CellLine(EntryNum(indx_id)).OX;
+                    RESULTS(i,2:end-1) = CelloDATA.CellData(EntryNum(indx_id),ResultsIndx);
                     RESULTS(i,end) = "Multiple match to synonym, one match cleaned primary Id";
                 else
                     RESULTS(i,end) = "MULTIPLE MATCHES!";
@@ -89,24 +137,18 @@ for i = 1:n
         else
             indx = strcmpi(Id,PrimaryId) & indx_human;
             if sum(indx) == 1 % Only one unique Hit
-                RESULTS(i,2) = PrimaryId(indx);
-                % RESULTS(i,3) = CelloDATA.CellLine(indx).AC;
-                % RESULTS(i,4) = CelloDATA.CellLine(indx).OX;
+                RESULTS(i,2:end-1) = CelloDATA.CellData(indx,ResultsIndx);
                 RESULTS(i,end) = "Unique match to Primary Id, ignoring case";
             else
                 indx = strcmpi(Id,CelloDATA.Synonyms); % Case insensative
                 if sum(indx) == 1 || isscalar(unique((CelloDATA.SynonymsIndx(indx))))% Only one unique Hit
                     EntryNum = CelloDATA.SynonymsIndx(indx);
-                    RESULTS(i,2) = PrimaryId(EntryNum(1));
-                    % RESULTS(i,3) = CelloDATA.CellLine(EntryNum).AC;
-                    % RESULTS(i,4) = CelloDATA.CellLine(EntryNum).OX;
+                    RESULTS(i,2:end-1) = CelloDATA.CellData(EntryNum(1),ResultsIndx);
                     RESULTS(i,end) = "Unique match to synonym, ignoring case";
                 else
                     indx = strcmpi(Id_Clean,PrimaryIdClean) & indx_human ;
                     if sum(indx) == 1 % Only one unique Hit
-                         RESULTS(i,2) = PrimaryId(indx);
-                        % RESULTS(i,3) = CelloDATA.CellLine(indx).AC;
-                        % RESULTS(i,4) = CelloDATA.CellLine(indx).OX;
+                        RESULTS(i,2:end-1) = CelloDATA.CellData(indx,ResultsIndx);
                         RESULTS(i,end) = "Primary Id matched with cleaned Id";
                     else
                         RESULTS(i,end) = "NO MATCH FOUND";
@@ -115,4 +157,20 @@ for i = 1:n
             end
         end
     end
+end
+
+if PrintResults
+    [fid,message] = fopen(FileOut,'w');
+    if  fid == -1
+        disp(FileOut)
+        disp(message)
+        return
+    end
+    fprintf(fid,'%s\t',OutputFields{:});
+    fprintf(fid,'\n');
+    for i = 1:n
+        fprintf(fid,'%s\t',RESULTS(i,:));
+        fprintf(fid,'\n');
+    end
+    fclose(fid);
 end
--- ProcessCellosaurusFile.m
+++ ProcessCellosaurusFile.m
@@ -1,4 +1,47 @@
 function DATA = ProcessCellosaurusFile(FileIn)
+% USAGE:
+%   fh  = PlotBoxPlotDATA(DATA,VariableId,GroupVariableName,GroupsToUse,varargin)
+%   creates a scatter boxplot plot from DATA structure
+%
+% INPUTS:
+% * DATA:       DATA structure w
+% * VariableId: Id to be used for groups along the X-axis from RowAnnotationFields.
+% * GroupVariableName:    List of Groups to be used and order for the X-axis, [] uses all
+% * GroupsToUse:    Id to be used for groups along the Y-axis from RowAnnotationFields.
+%
+% OUTPUTS:
+%   fh: Figure handle to dot plot figure
+%
+%   options ---------------------------------------
+%
+%   'FontSize'          FontSize for all text in figure [7]
+%   'FigSize'           Vector with figure width and hight in inches [ [2 3.5] ]
+%   'MarkerSize'        Marker size for scatter points [30]
+%   'MarkerLineWidth'   Marker line width for scatter points [1]
+%   'MarkerType'        Marker types for scatter points,{'o','d','^'} [{'o'}]
+%   'MarkerFaceColor'   Marker edge color for scatter points ['none']
+%   'ColorMap'          Marker edge color for scatter points [GetPalette('Science')]
+%   'XJitterWidth'      Width for scatter points [0.6]
+%   'BoxLineWidth'      LineWidth for boxes [1]
+%   'BoxColor'          Color for box lines [ [0 0 0] ]
+%   'BoxWidth'          Width for boxes [0.8]
+%   'YlabelTxt'         Text to be used for Y label, if not defined VariableId will be used
+%   'TitleTxt'          text for Title
+%   'CalcStats'         Calculate stats between groups, Nx2 matrix defines comparisons, [] all
+%   'StatType'          Type of comparison, Mann Whitney (MW) or T-test (t-test) ['MW']
+%   'PlotStars'         Use starts instead of p-values
+%   'StatLineWidth'     Line witth for line btween groups [0.5]
+%   'Show_NS'           Show not significant results [false]
+%   'TargetAxes'        Axes handle for target axist []
+%   'XTickAngle'        Angle for X ticks [-45]
+%   'SortData'          Sort group order based on median value
+%   'MultipleY'         How to summarize multiple y's Mean' or 'PCA' ['Mean']
+%   'DataTipId'         Id to be used for datatip 'RowId' or id from 'RowAnnotationFields' ['RowId']
+
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% by Anders Berglund, 2025 aebergl at gmail.com                           %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 Terminator = "//";
 DATA.DateDownloaded = "";
 %Read already downloaded 'EBPlusPlusAdjustPANCAN_IlluminaHiSeq_RNASeqV2.geneExp.tsv'
