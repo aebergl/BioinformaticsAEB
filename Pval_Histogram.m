db433901159053cda7a0bd44751e4b16c22ad4d2
aebergl <aebergl@gmail.com>
2026-05-01 18:00:44 -0400

    Update Pval_Histogram.m

--- Pval_Histogram.m
+++ Pval_Histogram.m
@@ -1,19 +1,25 @@
 function fh = Pval_Histogram(P_values,nBins,Cmap,TitleTxt)
-fh = figure('Name','p-value histogram','Color','w','Tag','p-value histogram','GraphicsSmoothing','off');
-ah=axes(fh,'NextPlot','add','tag','p-value histogram','Box','off','FontSize',16,'Linewidth',1);
+FontSize = 8;
+Linewidth = 0.5;
+FigureSize = [3,2];
 
-hh=histogram(ah,P_values,nBins,'LineWidth',1,'Normalization','count','FaceColor',Cmap);
+fh = figure('Name','p-value histogram','Color','w','Tag','p-value histogram','Units','inches');
+fh.Position(3:4) = FigureSize;
+ah=axes(fh,'NextPlot','add','tag','p-value histogram','Box','off','FontSize',FontSize,'Linewidth',1);
+
+hh=histogram(ah,P_values,nBins,'LineWidth',Linewidth,'Normalization','count','FaceColor',Cmap,...
+    'FaceAlpha',0.9);
 ah.XLabel.String = {'p-value'};
-ah.XLabel.FontSize = 18;
+ah.XLabel.FontSize = FontSize;
 ah.XLim = [-0.05 1.05];
 ah.XTick = 0:0.1:1;
-
+ah.XTickLabelRotation = -45;
 ah.YLabel.String = 'Count';
-ah.YLabel.FontSize = 18;
+ah.YLabel.FontSize = FontSize;
 ah.YTick = [];
 
 if ~isempty(TitleTxt)
-   title(TitleTxt,'FontSize',20,'FontWeight','Normal')
+   title(TitleTxt,'FontSize',FontSize,'FontWeight','Normal')
     
 end
 
